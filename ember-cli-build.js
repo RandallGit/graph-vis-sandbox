c6c7f7407b56332d10160b0b29ed573a0e7170ce
relliott <randall.elliott@frogdesign.com>
2016-02-11 14:28:27 -0600

    Added several new layout algorithms

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -2,17 +2,10 @@
 /* global require, module */
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
-// var cytoscape = require('cytoscape');
-// var cycola = require('cytoscape-cola');
-// var cola = require('WebCola');
-//
-// cycola( cytoscape, cola ); // register extension
-
 module.exports = function(defaults) {
 	var app = new EmberApp(defaults, {
 		// Add options here
 	});
-
 	// Use `app.import` to add additional libraries to the generated
 	// output files.
 	//
@@ -25,9 +18,16 @@ module.exports = function(defaults) {
 	// modules that you would like to import into your application
 	// please specify an object with the list of modules as keys
 	// along with the exports of each module as its value.
-	app.import("bower_components/cytoscape/dist/cytoscape.min.js");
+	app.import("bower_components/cytoscape/dist/cytoscape.js");
+	app.import("bower_components/webcola/WebCola/cola.js");
 	app.import("bower_components/cytoscape-cola/cytoscape-cola.js");
-	app.import("bower_components/webcola/WebCola/cola.min.js");
 
 	return app.toTree();
+
+	var cytoscape = require('cytoscape');
+	var cycola = require('cytoscape-cola');
+	var cola = require('webcola');
+
+	cycola( cytoscape, cola ); // register extension
+
 };
